1507e48f83b747fda81b7288cba3c97b9b728e98
Sorany-Potato <64361942+Sorany-Potato@users.noreply.github.com>
2021-01-07 17:46:40 +0900

    修正

--- ai_nock59.py
+++ ai_nock59.py
@@ -12,24 +12,40 @@ import numpy as np
 from tqdm import tqdm
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from matplotlib import pyplot as plt
 
 log=ai_def.Lg()
 
-def over(C,class_weight):
+def over(C,solver,class_weight):
     clf = LogisticRegression(C=C, solver=solver, class_weight=class_weight)
     clf.fit(log['train_feature'], log['train_category'])
     train_sc=ai_def.estimation(clf, log['train_feature'])
     valid_sc=ai_def.estimation(clf, log['valid_feature'])
     test_sc=ai_def.estimation(clf, log['test_feature'])
     result=[]
-    result.append(accuracy_score(log['train_category'],train_sc))
-    result.append(accuracy_score(log['valid_category'],valid_sc))
-    result.append(accuracy_score(log['test_category'],test_sc))
+    train_accuracy = accuracy_score(log['train_category'],train_sc)
+    valid_accuracy = accuracy_score(log['valid_category'],valid_sc)
+    test_accuracy = accuracy_score(log['test_category'],test_sc)
+
+    result.append([C,train_accuracy, valid_accuracy, test_accuracy])
     return result
 
-C=np.longspace(-5,4,10,base=10)
+C=np.logspace(-5,4,10,base=10)
 class_weight=[None, 'balanced']
+solver=['newton-cg','lbfgs','libliner','sag','saga']
 best_parameter=None
-best_scores=None
-max_valid_score=0
+best_score=None
+max_score=0
+
+for c in C:
+    for solve in solver:
+        for weight in class_weight:
+            print(c, solve, weight)
+            result=over(c,solve,weight)
+            if result[2] > max_score:
+                max_score=result[2]
+                best_parameter=[c,solve,weight]
+                best_score=result
+                ai_def.plot(result)
+print(best_parameter)
+print(best_score)
+print(best_score[3])
